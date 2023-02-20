2d0a1f206e9569fe91827f88a948c98c6960ede1
Rob Prouse <rob@prouse.org>
2023-02-19 22:27:58 -0500

    Dynamically create map

--- src/StateGame.c
+++ src/StateGame.c
@@ -3,13 +3,18 @@
 #include "ZGBMain.h"
 #include "Scroll.h"
 #include "SpriteManager.h"
+#include "Levels.h"
 
 IMPORT_MAP(map);
 
-void START() {
-	scroll_target = SpriteManagerAdd(SpritePlayer, 50, 50);
+void START() 
+{
+	//scroll_target = SpriteManagerAdd(SpritePlayer, 50, 50);
 	InitScroll(BANK(map), &map, 0, 0);
+  //set_bkg_data(0, 20 * 18, Levels);
+  set_bkg_tiles(0, 0, 8, 10, Levels);
 }
 
-void UPDATE() {
+void UPDATE() 
+{
 }
